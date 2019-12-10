c1f6bbbc77d99447ebf29e31fc9c2d8d95ccc9a2
alexandrejls <57766439+alexandrejls@users.noreply.github.com>
2019-12-10 10:50:51 -0300

    Update repositorio.sh

--- repositorio.sh
+++ repositorio.sh
@@ -4,22 +4,25 @@ echo -e "Atualizando as listas do Apt, aguarde..."
 	#opção do comando: &>> (redirecionar a saída padrão)
 	apt update 
 echo -e "Listas atualizadas com sucesso!!!"
-sleep 1
+sleep 3
 echo
+clear
 #
 #
 echo -e "Baixando e instalando atualizações, aguarde..."
 	#opção do comando: &>> (redirecionar a saída padrão)
 	yes | apt upgrade 
 echo -e "Listas atualizadas com sucesso!!!"
-sleep 1
+sleep 3
 echo
+clear
 #
 echo -e "Removendo software desnecessários, aguarde..."
 	# opção do comando: &>> (redirecionar a saída padrão)
 	# opção do comando apt: -y (yes)
 	apt -y autoremove 
 echo -e "Software removidos com sucesso!!!"
-sleep 1
+sleep 3
 echo
+clear
 #
