c7cf43491d66cc1f114ba605263454999601c471
alexandrejls <57766439+alexandrejls@users.noreply.github.com>
2019-12-10 10:43:40 -0300

    Update repositorio.sh

--- repositorio.sh
+++ repositorio.sh
@@ -1,21 +1,16 @@
 #!/bin/bash
-echo -e "Adicionando o Repositório Universal do Apt..."
-	# opção do comando: &>> (redirecionar a saída padrão)
-	add-apt-repository universe 
-echo -e "Repositório adicionado com sucesso!!!"
-sleep 5
-echo
-#
-echo -e "Adicionando o Repositório Multiversão do Apt..."
-	# opção do comando: &>> (redirecionar a saída padrão)
-	add-apt-repository multiverse 
-echo -e "Repositório adicionado com sucesso!!!"
-sleep 5
-echo
 #
 echo -e "Atualizando as listas do Apt, aguarde..."
 	#opção do comando: &>> (redirecionar a saída padrão)
 	apt update 
+echo -e "Listas atualizadas com sucesso!!!"
+sleep 1
+echo
+#
+#
+echo -e "Baixando e instalando atualizações, aguarde..."
+	#opção do comando: &>> (redirecionar a saída padrão)
+	apt upgrade 
 echo -e "Listas atualizadas com sucesso!!!"
 sleep 1
 echo
