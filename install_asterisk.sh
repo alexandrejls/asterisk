6aabf776c3dc892d448b262549a385fe71ff1688
alexandrejls <57766439+alexandrejls@users.noreply.github.com>
2019-12-10 13:06:28 -0300

    Update install_asterisk.sh

--- install_asterisk.sh
+++ install_asterisk.sh
@@ -2,9 +2,8 @@
 echo -e "Instalando as dependências do Asterisk, aguarde..."
 	# opção do comando: &>> (redirecionar a saída padrão)
 	# opção do comando apt: -y (yes) | $(uname -r) = kernel-release, \ (bar left) quedra de linha na opção do apt
-	apt install -y build-essential libssl-dev libelf-dev libncurses5-dev libnewt-dev libxml2-dev linux-headers-$(uname -r) \
-	libsqlite3-dev uuid-dev subversion libjansson-dev sqlite3 autoconf automake libtool libedit-dev flex bison libtool \
-	libtool-bin unzip sox openssl zlib1g-dev unixodbc unixodbc-dev vim
+	apt install -y build-essential libssl-dev libelf-dev libncurses5-dev libnewt-dev libxml2-dev 
+	apt install -y make wget openssl ncurses-base newt-tcl libxml2-dev gcc sqlite
 echo -e "Dependências instaladas com sucesso!!!"
 sleep 3
 echo
