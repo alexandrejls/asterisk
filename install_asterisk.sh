5e13f3d907b569bc214824c93b466746176e96d4
alexandrejls <57766439+alexandrejls@users.noreply.github.com>
2019-12-10 13:25:10 -0300

    Update install_asterisk.sh

--- install_asterisk.sh
+++ install_asterisk.sh
@@ -14,7 +14,7 @@ echo -e "Instalando as dependências do Asterisk, aguarde..."
 	# opção do comando apt: -y (yes) | $(uname -r) = kernel-release, \ (bar left) quedra de linha na opção do apt
 	apt install -y build-essential libssl-dev libelf-dev libncurses5-dev libnewt-dev libxml2-dev 
 	apt install -y make wget openssl ncurses-base newt-tcl libxml2-dev gcc sqlite mysql-server
-echo -e "Dependências instaladas com sucesso!!!"
+echo -e "Dependências OK!!!"
 sleep 3
 echo
 #
@@ -36,7 +36,7 @@ echo -e "Download e instalação do DAHDI, aguarde..."
 	make install 
 	# opção do comando cd: .. (dois pontos sequenciais - Subir uma pasta)
 	cd ..
-echo -e "DAHDI instalado com sucesso!!!, continuando com o script..."
-sleep 5
+echo -e "DAHDI, OK..."
+sleep 3
 echo	
 #
