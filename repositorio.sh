c36c26f195af64db41862b5edc5ba73637d72b8d
alexandrejls <57766439+alexandrejls@users.noreply.github.com>
2019-12-10 10:33:58 -0300

    Add files via upload

---
+++ repositorio.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+echo -e "Adicionando o Repositório Universal do Apt..."
+	# opção do comando: &>> (redirecionar a saída padrão)
+	add-apt-repository universe &>> $LOG
+echo -e "Repositório adicionado com sucesso!!!"
+sleep 5
+echo
+#
+echo -e "Adicionando o Repositório Multiversão do Apt..."
+	# opção do comando: &>> (redirecionar a saída padrão)
+	add-apt-repository multiverse &>> $LOG
+echo -e "Repositório adicionado com sucesso!!!"
+sleep 5
+echo
+#
+echo -e "Atualizando as listas do Apt, aguarde..."
+	#opção do comando: &>> (redirecionar a saída padrão)
+	apt update &>> $LOG
+echo -e "Listas atualizadas com sucesso!!!"
+sleep 5
+echo
+#
