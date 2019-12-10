9eb7d6effa56d4c4febb658b574d1487073dfa0e
alexandrejls <57766439+alexandrejls@users.noreply.github.com>
2019-12-10 13:45:22 -0300

    Update install_asterisk.sh

--- install_asterisk.sh
+++ install_asterisk.sh
@@ -4,7 +4,6 @@ export DEBIAN_FRONTEND="noninteractive"
 DAHDI="http://downloads.asterisk.org/pub/telephony/dahdi-linux/dahdi-linux-current.tar.gz"
 DAHDITOOLS="http://downloads.asterisk.org/pub/telephony/dahdi-tools/dahdi-tools-current.tar.gz"
 LIBPRI="http://downloads.asterisk.org/pub/telephony/libpri/libpri-current.tar.gz"
-ASTERISK="http://downloads.asterisk.org/pub/telephony/asterisk/asterisk-17-current.tar.gz"
 PTBRCORE="https://www.asterisksounds.org/pt-br/download/asterisk-sounds-core-pt-BR-sln16.zip"
 PTBREXTRA="https://www.asterisksounds.org/pt-br/download/asterisk-sounds-extra-pt-BR-sln16.zip"
 SOUNDS="/var/lib/asterisk/sounds/pt_BR"
@@ -19,25 +18,9 @@ echo -e "Dependências OK!!!"
 sleep 3
 echo
 #
-echo -e "Download e instalação do DAHDI, aguarde..."
-	# opção do comando: &>> (redirecionar a saída padrão)
-	# opção do comando wget: -O (file)
-	wget -O dahdi-linux.tar.gz $DAHDI
-	# opção do comando tar: -z (gzip), -x (extract), -v (verbose), -f (file)
-	tar -zxvf dahdi-linux.tar.gz
-	# acessando diretório do dahdi-linux
-	cd dahdi-linux*/ 
-	# preparação e configuração do source para compilação
-	./configure 
-	# desfaz o processo de compilação anterior
-	make clean  
-	# compila todas as opções do software
-	make all  
-	# executa os comandos para instalar o programa
-	make install 
-	# opção do comando cd: .. (dois pontos sequenciais - Subir uma pasta)
-	cd ..
-echo -e "DAHDI, OK..."
-sleep 3
-echo	
-#
+cd /usr/src
+ASTERISK="http://downloads.asterisk.org/pub/telephony/asterisk/asterisk-17-current.tar.gz"
+IF EXIST asterisk-17-current.tar.gz GOTO 1
+wget -O asterisk-17-current.tar.gz $ASTERISK
+:1
+
