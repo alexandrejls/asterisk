36ddc413979e2f5e93d3f0a6b47041a32352e9dd
alexandrejls <57766439+alexandrejls@users.noreply.github.com>
2020-01-03 09:43:19 -0300

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -7,13 +7,13 @@ apt update
 apt-get install build-essential wget libssl-dev libncurses5-dev libnewt-dev libxml2-dev linux-headers-$(uname -r) libsqlite3-dev uuid-dev git subversion libjansson-dev sqlite3 autoconf automake libtool libedit-dev -y
 echo "Entrando do diretório /usr/src:"
 cd /usr/src/
-DAHDITOOLS="http://downloads.asterisk.org/pub/telephony/dahdi-tools/dahdi-tools-current.tar.gz"
+
 PTBRCORE="https://www.asterisksounds.org/pt-br/download/asterisk-sounds-core-pt-BR-sln16.zip"
 PTBREXTRA="https://www.asterisksounds.org/pt-br/download/asterisk-sounds-extra-pt-BR-sln16.zip"
 LIBPRI="http://downloads.asterisk.org/pub/telephony/libpri/libpri-current.tar.gz"
 SOUNDS="/var/lib/asterisk/sounds/pt_BR"
 COUNTRYCODE="55"
-
+############################## DOWNLOAD E INSTALAÇÃO DO DAHDI ##############################
 DAHDI="http://downloads.asterisk.org/pub/telephony/dahdi-linux/dahdi-linux-current.tar.gz"
 echo -e "Download e instalação do DAHDI, aguarde..."
 if [ ! -f "/usr/src/DAHDI.tar.gz" ]; then
@@ -25,8 +25,27 @@ else
 fi
 	tar -zxvf DAHDI.tar.gz 
 	cd dahdi*/
-	make && make install && make config
+	./configure && make clean && make all && make install
 	cd ..
 echo -e "DAHDI instalado com sucesso!!!, continuando com o script..."
+############################## DOWNLOAD E INSTALAÇÃO DO DAHDI ##############################
+sleep 5
+echo
+############################## DOWNLOAD E INSTALAÇÃO DO DAHDITOOLS ##############################
+DAHDITOOLS="http://downloads.asterisk.org/pub/telephony/dahdi-tools/dahdi-tools-current.tar.gz"
+echo -e "Download e instalação do DAHDITOOLS, aguarde..."
+if [ ! -f "/usr/src/DAHDITOOLS.tar.gz" ]; then
+    echo "O arquivo não existe e será feito Download!!"
+    cd /usr/src/
+    wget -O DAHDITOOLS.tar.gz $DAHDI
+else
+    echo "O arquivo existe, não será feito Download!!"
+fi
+	tar -zxvf DAHDITOOLS.tar.gz 
+	cd dahdit*/
+	autoreconf -i && ./configure && make clean && make all && make install 
+	cd ..
+echo -e "DAHDITOOLS instalado com sucesso!!!, continuando com o script..."
+############################## DOWNLOAD E INSTALAÇÃO DO DAHDITOOLS ##############################
 sleep 5
 echo
