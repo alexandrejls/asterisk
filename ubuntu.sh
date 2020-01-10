535440183b4ddf22e310cb4107475b58aabd3071
alexandrejls <57766439+alexandrejls@users.noreply.github.com>
2020-01-10 13:47:39 -0300

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-echo "### Instalação do Asterisk no Ubuntu Server ###"
+echo "### Instalação do Asterisk ###"
 export DEBIAN_FRONTEND="noninteractive"
-add-apt-repository multiverse 
+add-apt-repository multiverse
 add-apt-repository universe
 LOG="/var/log/$(echo $0 | cut -d'/' -f2)"
-sleep 5
+sleep 3
 apt update &>> $LOG
 apt -y upgrade &>> $LOG
 apt-get install -y build-essential wget libssl-dev libncurses5-dev libnewt-dev libxml2-dev linux-headers-$(uname -r) &>> $LOG
@@ -13,13 +13,15 @@ apt-get install -y libelf-dev libsqlite3-dev flex bison unzip sox openssl zlib1g
 apt install -y build-essential libssl-dev libelf-dev libncurses5-dev libnewt-dev libxml2-dev  &>> $LOG
 apt install -y make wget openssl ncurses-base newt-tcl libxml2-dev gcc sqlite mysql-server &>> $LOG
 apt install -y config-package-dev configure-debian &>> $LOG
+apt install -y build-essential libssl-dev libelf-dev libncurses5-dev libnewt-dev libxml2-dev linux-headers-$(uname -r) &>> $LOG
+apt install -y libsqlite3-dev uuid-dev subversion libjansson-dev sqlite3 autoconf automake libtool libedit-dev flex bison libtool &>> $LOG
+apt install -y libtool-bin unzip sox openssl zlib1g-dev unixodbc unixodbc-dev vim git &>> $LOG
+apt install -y gcc g++ build-essential dkms libedit-dev flex bison libtool libtonezone-dev pkg-config &>> $LOG
+sleep 3
 echo "Entrando do diretório /usr/src:"
 cd /usr/src/
 
-PTBRCORE="https://www.asterisksounds.org/pt-br/download/asterisk-sounds-core-pt-BR-sln16.zip"
-PTBREXTRA="https://www.asterisksounds.org/pt-br/download/asterisk-sounds-extra-pt-BR-sln16.zip"
-SOUNDS="/var/lib/asterisk/sounds/pt_BR"
-COUNTRYCODE="55"
+
 ############################## DOWNLOAD E INSTALAÇÃO DO DAHDI ##############################
 DAHDI="http://downloads.asterisk.org/pub/telephony/dahdi-linux/dahdi-linux-current.tar.gz"
 echo -e "Download e instalação do DAHDI, aguarde..."
@@ -40,7 +42,7 @@ fi
 	make all &>> $LOG 
 	make install
 	make config &>> $LOG 
-	cd ..
+	cd ..c
 echo -e "DAHDI instalado com sucesso!!!, continuando com o script..."
 ############################## DOWNLOAD E INSTALAÇÃO DO DAHDI ##############################
 sleep 5
@@ -96,6 +98,7 @@ echo
 ############################## DOWNLOAD E INSTALAÇÃO DO ASTERISK ##############################
 echo -e "Download e instalação do ASTERISK, aguarde..."
 ASTERISK="http://downloads.asterisk.org/pub/telephony/asterisk/asterisk-17.1.0.tar.gz"
+COUNTRYCODE="55"
 if [ ! -f "/usr/src/lasterisk-17.1.0.tar.gz" ]; then
     echo "O arquivo não existe e será feito Download!!"
     cd /usr/src/
@@ -111,9 +114,43 @@ fi
 	make 
 	make install 
 	make config
+	make samples
+	make basic-pbx
+	make progdocs
 echo -e "ASTERISK instalado com sucesso!!!, continuando com o script..."
 ############################## DOWNLOAD E INSTALAÇÃO DO ASTERISK ##############################
-
+echo   "Resolvendo as dependências do suporte a Música e Sons em MP3, aguarde..."
+	bash contrib/scripts/get_mp3_source.sh  
+echo   "Resolvendo as dependências do suporte ao Codec iLBC, aguarde..."
+	bash contrib/scripts/get_ilbc_source.sh  
+echo   "Instalando as dependência do MP3 e ILBC utilizando o debconf-set-selections, aguarde..."
+	# opção do comando | (piper): (Conecta a saída padrão com a entrada padrão de outro comando)
+	echo "libvpb1 libvpb1/countrycode $COUNTRYCODE" | debconf-set-selections  
+echo   "Install_prereq install, aguarde..."
+	yes | bash contrib/scripts/install_prereq install
+echo   "Instala um conjunto de scripts de configuração dos Logs do Asterisk (rsyslog)"
+	make install-logrotate 
+	# inicializando o serviço do Asterisk
+	systemctl start asterisk
+	cd ..
+echo -e "Asterisk instalado com sucesso!!!, continuando com o script..."
+sleep 5
+echo -e "Download e configuração do Sons em Português/Brasil do Asterisk, aguarde..."
+	SOUNDS="/var/lib/asterisk/sounds/pt_br"
+	mkdir -v $SOUNDS
+	cp -v conf/asterisk/convert.sh $SOUNDS &>> $LOG
+	cd $SOUNDS &>> $LOG
+	PTBRCORE="https://www.asterisksounds.org/pt-br/download/asterisk-sounds-core-pt-BR-sln16.zip"
+	PTBREXTRA="https://www.asterisksounds.org/pt-br/download/asterisk-sounds-extra-pt-BR-sln16.zip"
+	wget -O core.zip $PTBRCORE &>> $LOG
+	wget -O extra.zip $PTBREXTRA &>> $LOG  
+	unzip -o core.zip &>> $LOG
+	unzip -o extra.zip &>> $LOG 
+	bash convert.sh &>> $LOG 
+	cd - 
+echo -e "Configuração do Sons em Português/Brasil feito com sucesso!!!!, continuado com o script..."
+sleep 5
+echo
 
 
 
